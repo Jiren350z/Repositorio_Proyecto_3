d2ef2d782f010e7e9efe7f11b2f3ba407998d08f
Jiren350z <127953532+Jiren350z@users.noreply.github.com>
2023-07-09 22:56:04 -0400

    probando los 7 algoritmos solicitados

--- Carrera_de_Algoritmos_Diego_Morales.cpp
+++ Carrera_de_Algoritmos_Diego_Morales.cpp
@@ -157,6 +157,34 @@ void MergeSort(vector<int>& arr, int left, int right)
         merge(arr, left, middle, right);
     }
 }
+
+// Función para colocar el pivote en su posición correcta
+int partition(std::vector<int>& arr, int low, int high) {
+    int pivot = arr[high];
+    int i = low - 1;
+
+    for (int j = low; j <= high - 1; j++) {
+        if (arr[j] < pivot) {
+            i++;
+            std::swap(arr[i], arr[j]);
+        }
+    }
+
+    std::swap(arr[i + 1], arr[high]);
+    return i + 1;
+}
+
+// Función recursiva para ordenar el arreglo utilizando Quick Sort
+void quickSort(std::vector<int>& arr, int low, int high) {
+    if (low < high) {
+        int pivotIndex = partition(arr, low, high);
+
+        // Ordenar los elementos antes y después del pivote
+        quickSort(arr, low, pivotIndex - 1);
+        quickSort(arr, pivotIndex + 1, high);
+    }
+}
+
 //determina el resultado en tiempo de los algoritmos de ordenamiento
 double getResultFromAlg(vector<int>& arr,int option) 
 {
